dbee26a42a000a97925e3f89157024fbf803bd86
viddik13 <viddik13@gmail.com>
2016-03-16 19:35:56 +0300

    Changed the database model and migrated.

--- app/models.py
+++ app/models.py
@@ -45,11 +45,3 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post %r>' % (self.body)
-        
-class OUser(db.Model)
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(40), unique = True)
-    email = db.Column(db.String(80), unique = True)
-    
-    def __repr__(self)
-        return '<OUser %r>' % (self.email)
--- db_migrate.py
+++ db_migrate.py
@@ -1,4 +1,4 @@
-#!flask/bin/python
+#!flask/Scripts/python
 import imp
 from migrate.versioning import api
 from app import db
