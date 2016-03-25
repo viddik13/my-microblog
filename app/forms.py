bfa0c3cdc1b9e6af3477322a4cbabb6cddbad636
viddik <viddik13@gmail.com>
2016-03-25 12:41:06 +0300

    Too much changes. Need to merge.

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,12 @@
 from flask.ext.wtf import Form
-from wtforms import TextField, BooleanField
-from wtforms.validators import Required
+from wtforms import StringField, BooleanField, TextAreaField
+from wtforms.validators import DataRequired, Length
 
 class LoginForm(Form):
-    username = TextField('username', validators = [Required()])
-    email = TextField('email', validators = [Required()])
+    username = StringField('username', validators = [DataRequired()])
+    email = StringField('email', validators = [DataRequired()])
     remember_me = BooleanField('remember_me', default = False)
+
+class ProfileEditForm(Form):
+    nickname = StringField('nickname', validators=[DataRequired()])
+    about_me = TextAreaField('about_me', validators=[Length(min=0, max=140)])
--- app/views.py
+++ app/views.py
@@ -3,9 +3,10 @@ from flask import url_for, request, g
 from flask.ext.login import login_user, logout_user
 from flask.ext.login import current_user, login_required
 from app import app, db, lm
-from .forms import LoginForm
+from .forms import LoginForm, ProfileEditForm
 from .models import User
 from datetime import datetime
+
 
 @app.route('/')
 @app.route('/index')
@@ -14,60 +15,55 @@ def index():
     user = g.user
     posts = [
         {
-            'author': { 'nickname': 'Feofan' },
+            'author': {'nickname': 'Feofan'},
             'body': 'It\'s cold in the forest!!'
         },
         {
-            'author': { 'nickname': 'Feofan' },
+            'author': {'nickname': 'Feofan'},
             'body': "Better dance some D'n'B!!"
         }
     ]
 
-    return render_template("index.html",
-                           title = 'Home',
-                           user = user,
-                           posts = posts)
+    return render_template("index.html", title='Home', user=user, posts=posts)
 
-@app.route('/login', methods = ['GET', 'POST'])
-#@oid.loginhandler
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     if g.user is not None and g.user.is_authenticated:
         return redirect(url_for('index'))
 
     l_form = LoginForm()
-    
+
     if l_form.validate_on_submit():
         session['remember_me'] = l_form.remember_me.data
-        nickname = l_form.username.data
-        email = l_form.email.data
+        nickname, email = l_form.username.data, l_form.email.data
 
         user = User.query.filter_by(email=email).first()
 
         if user is None:
 
             if nickname is None or nickname == "":
-                nickname = email.splint('@')[0]
+                nickname = email.split('@')[0]
 
-            new_user = User(nickname=nickname, email=email)
+            user = User(nickname=nickname, email=email)
             print "***Database operation: adding user..."
-            db.session.add(new_user)
+            db.session.add(user)
             db.session.commit()
-            user = new_user
 
         remember_me = False
         if 'remember_me' in session:
             remember_me = session['remember_me']
             session.pop('remember_me', None)
 
-        login_user(user)
+        login_user(user, remember_me)
 
         return redirect(request.args.get('next') or url_for('index'))
-        #return oid.try_login(l_form.openid.data, ask_for=['nickname', 'email'])
 
     return render_template("login.html",
-                           title = "Sign In",
-                           form = l_form,
-                           providers = app.config['OPENID_PROVIDERS'])
+                           title="Sign In",
+                           form=l_form,
+                           providers=app.config['OPENID_PROVIDERS'])
+
 
 @app.before_request
 def before_request():
@@ -77,46 +73,24 @@ def before_request():
         db.session.add(g.user)
         db.session.commit()
 
-#@oid.after_login
-# def after_login(resp):
-#     if resp.email is None or resp.email == "":
-#         print "***after_login: no email"
-#         flash('Invalid login. Please try again.')
-#         return redirect(url_for('login'))
-#
-#     user = User.query.filter_by(email=resp.email).first()
-#     if user is None:
-#         nickname = resp.nickname
-#         if nickname is None or nickname == "":
-#             nickname = resp.email.splint('@')[0]
-#         user = User(nickname=nickname, email=resp.email)
-#         print "***Database operation: adding user..."
-#         db.session.add(user)
-#         db.session.commit()
-#
-#     remember_me = False
-#     if 'remember_me' in session:
-#         remember_me = session['remember_me']
-#         session.pop('remember_me', None)
-#
-#     login_user(user, remember = remember_me)
-#     return redirect(request.args.get('next') or url_for('index'))
-    
+
 @lm.user_loader
-def load_user(id):
-    return User.query.get(int(id))
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 
 @app.route('/logout')
 def logout():
         logout_user()
         return redirect(url_for('index'))
 
+
 @app.route('/user/<nickname>')
 @login_required
 def user(nickname):
     user = User.query.filter_by(nickname=nickname).first()
-    if user == None:
-        flash('User % not found.' % nickname)
+    if user is None:
+        flash('User %r not found.' % nickname)
         return redirect(url_for('index'))
 
     posts = [
@@ -127,3 +101,12 @@ def user(nickname):
     return render_template('user.html',
                            user=user,
                            posts=posts)
+
+
+@app.route('/edit', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    edit_form = ProfileEditForm()
+    if edit_form.validate_on_submit():
+        g.user.nickname = edit_form.nickname.data
+        g.user.about_me = edit_form.about_me.data
